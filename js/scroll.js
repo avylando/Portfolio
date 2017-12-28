fc14c512ac1efe5a4829db8879e9475c4de186a1
Anatoly Dolgov <anatoldd@list.ru>
2017-12-28 17:31:21 +0500

    continue development

--- js/scroll.js
+++ js/scroll.js
@@ -2,7 +2,7 @@
 
 (function () {
   console.log('sss');
-  var skillsSection = $('.skills__wrapper');
+  var advantagesSection = $('.advantages__wrapper');
   var sliderSection = $('.portfolio__wrapper');
   var aboutMeSection = $('.about-me__wrapper');
   console.log(sliderSection);
@@ -10,13 +10,13 @@
     $(window).scroll(function () {
       var scrollValue = $(document).scrollTop();
       console.log(scrollValue);
-      if (scrollValue > 400 && scrollValue < 1100) {
-        window.debounce(skillsSection.animate({ 'opacity': '1' }, 1000), 500);
+      if (scrollValue > 150 && scrollValue < 1100) {
+        window.debounce(advantagesSection.animate({ 'opacity': '1' }, 1000), 500);
       } else {
-        window.debounce(skillsSection.css('opacity', '0'), 200);
+        window.debounce(advantagesSection.css('opacity', '0'), 500);
       }
 
-      if (scrollValue > 750 && scrollValue < 2100) {
+      if (scrollValue > 600 && scrollValue < 2200) {
         // sliderSection.animate({
         //   step: function (now, fx) {
         //     console.log(now);
@@ -24,20 +24,20 @@
         //   }, duration: 'slow'
         // }, 'linear' );
         if (sliderSection.hasClass('portfolio--hide')) {
-          window.debounce(sliderSection.removeClass('portfolio--hide'), 100);
+          window.debounce(sliderSection.removeClass('portfolio--hide'), 500);
         }
-        window.debounce(sliderSection.addClass('portfolio--show'), 200);
+        window.debounce(sliderSection.addClass('portfolio--show'), 500);
       } else if (sliderSection.hasClass('portfolio--show')) {
-        window.debounce(sliderSection.removeClass('portfolio--show'), 200);
+        window.debounce(sliderSection.removeClass('portfolio--show'), 500);
       }
 
-      if (scrollValue > 1500) {
+      if (scrollValue > 1300) {
         if (aboutMeSection.hasClass('about-me--hide')) {
-          window.debounce(aboutMeSection.removeClass('about-me--hide'), 100);
+          window.debounce(aboutMeSection.removeClass('about-me--hide'), 500);
         }
-        window.debounce(aboutMeSection.addClass('about-me--show'), 200);
+        window.debounce(aboutMeSection.addClass('about-me--show'), 500);
       } else if (aboutMeSection.hasClass('about-me--show')) {
-        window.debounce(aboutMeSection.removeClass('about-me--show'), 200);
+        window.debounce(aboutMeSection.removeClass('about-me--show'), 500);
       }
     })
   })
